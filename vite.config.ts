14ab1b3ff015054c5d8436b47fcaf870a8ac22d8
Wanderson de Araujo Freitas <wanderson.freitas@benner.com.br>
2026-06-20 22:49:11 -0300

    dev

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/waf-maju/', // GitHub Pages: https://wandersonafreitas.github.io/waf-maju/
   plugins: [
     react(),
     tailwindcss(),
